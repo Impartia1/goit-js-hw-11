bd853d384379477f915cd2c5cef42e34fb601e3c
Dondik Artem <108674705+Impartia1@users.noreply.github.com>
2023-06-10 21:49:09 +0300

    second try

--- src/index.js
+++ src/index.js
@@ -15,51 +15,103 @@ loadBtn.addEventListener('click', onLoadBtnClick);
 
 let formValue = '';
 
-function onFormSubmit(evt) {
+// loadBtn.classList.add('is-hidden')
+
+
+async function onFormSubmit(evt) {
   evt.preventDefault();
+  loadBtn.classList.add('is-hidden')
   formValue = evt.target.elements.searchQuery.value.trim();
   if (formValue === '') {
     Notify.failure(
       'Sorry, there are no images matching your search query. Please try again.'
     );
+    
     return;
   }
-  fetchData(formValue)
-    .then(data => {
-      if (data.data.hits.length === 0) {
-        Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-        return;
-      }
-      Notify.info(`Hooray! We found ${data.data.totalHits} images.`);
-      let markup = renderMarkup(data.data.hits);
+  try {
+    const data = await fetchData(formValue);
+    if (data.data.hits.length === 0) {
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      gallery.innerHTML = '';
+      return;
+    }
+    Notify.info(`Hooray! We found ${data.data.totalHits} images.`);
+    let markup = renderMarkup(data.data.hits);
 
-      gallery.innerHTML = markup;
-      lightbox.refresh();
-      loadBtn.style.display = 'block';
-    })
-    .catch(error => console.log(error.message));
+    gallery.innerHTML = markup;
+    lightbox.refresh();
+
+    if (data.data.totalHits > 40) {
+      loadBtn.classList.remove('is-hidden');
+    }
+    
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
-function onLoadBtnClick() {
-  fetchData(formValue).then(data => {
-    if (data.data.hits.length === 0) {
+async function onLoadBtnClick() {
+  try {
+    const data = await fetchData(formValue);
+    if (data.data.hits.length >= 0 && data.data.hits.length < 40) {
       Notify.info("We're sorry, but you've reached the end of search results.");
       loadBtn.style.display = 'none';
+      let markup = renderMarkup(data.data.hits);
+      gallery.insertAdjacentHTML('beforeend', markup);
+      lightbox.refresh();
+      const { height: cardHeight } = document.querySelector('.gallery').firstElementChild.getBoundingClientRect();
+
+      window.scrollBy({
+        top: cardHeight * 2,
+        behavior: 'smooth',
+      });
       return;
     }
     let markup = renderMarkup(data.data.hits);
 
     gallery.insertAdjacentHTML('beforeend', markup);
     lightbox.refresh();
-    const { height: cardHeight } = document
-      .querySelector('.gallery')
-      .firstElementChild.getBoundingClientRect();
+    const { height: cardHeight } = document.querySelector('.gallery').firstElementChild.getBoundingClientRect();
 
     window.scrollBy({
       top: cardHeight * 2,
       behavior: 'smooth',
     });
-  });
+  } catch (error) {
+    console.log(error.message);
+  }
 }
+
+// function onLoadBtnClick() {
+//   fetchData(formValue).then(data => {
+//     if (data.data.hits.length >= 0 && data.data.hits.length < 40) {
+//       Notify.info("We're sorry, but you've reached the end of search results.");
+//       loadBtn.style.display = 'none';
+//       let markup = renderMarkup(data.data.hits);
+//       gallery.insertAdjacentHTML('beforeend', markup);
+//       lightbox.refresh();
+//       const { height: cardHeight } = document.querySelector('.gallery').firstElementChild.getBoundingClientRect();
+
+//     window.scrollBy({
+//       top: cardHeight * 2,
+//       behavior: 'smooth',
+//     });
+//       return;
+//     }
+//     let markup = renderMarkup(data.data.hits);
+
+//     gallery.insertAdjacentHTML('beforeend', markup);
+//     lightbox.refresh();
+//     const { height: cardHeight } = document
+//       .querySelector('.gallery')
+//       .firstElementChild.getBoundingClientRect();
+
+//     window.scrollBy({
+//       top: cardHeight * 2,
+//       behavior: 'smooth',
+//     });
+//   });
+// }
